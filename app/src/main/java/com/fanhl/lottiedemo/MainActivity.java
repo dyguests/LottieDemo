607a39145df7ca9f17f0e81d8b49cbae90549a32
fanhl <fanhonglin@klicen.com>
2017-05-05 13:56:00 +0800

    zx demo

--- app/src/main/java/com/fanhl/lottiedemo/MainActivity.java
+++ app/src/main/java/com/fanhl/lottiedemo/MainActivity.java
@@ -1,11 +1,14 @@
 package com.fanhl.lottiedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     private LottieAnimationView animationView;
 
@@ -18,16 +21,40 @@ public class MainActivity extends AppCompatActivity {
 
     private void assignViews() {
         animationView = ((LottieAnimationView) findViewById(R.id.animation_view));
+
+        animationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                Log.d(TAG, "onAnimationStart ");
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                Log.d(TAG, "onAnimationEnd");
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 
-    @Override protected void onResume() {
+    @Override
+    protected void onResume() {
         super.onResume();
 //        if (!animationView.isAnimating()) {
 //            animationView.playAnimation();
 //        }
     }
 
-    @Override protected void onPause() {
+    @Override
+    protected void onPause() {
         super.onPause();
 //        animationView.pauseAnimation();
     }
